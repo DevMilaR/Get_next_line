5bb5d50c4236a86c8a4981fd3a924f8e91707fb5
DevMilaR <161595710+DevMilaR@users.noreply.github.com>
2024-06-11 16:32:03 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lromano <lromano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 16:21:23 by lromano           #+#    #+#             */
+/*   Updated: 2024/06/11 16:22:04 by lromano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 48
 # endif
 
-# include <stdlib.h>
-# include <string.h>
-# include <stdio.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <fcntl.h>
+# include <limits.h>
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char	*storage, const char	*buffer);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
-char	*leftover(char *storage);
-char	*create_line(char *storage);
-size_t	ft_strlen(const char *str);
-
+char	*ft_free(char **str);
 #endif
